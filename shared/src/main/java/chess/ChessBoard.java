e084322ab6e69263282a8f25b0a456412b13552c
Nathan Preslar <nathanpreslar@Nathans-MBP-35.app.byu.edu>
2024-09-16 18:39:50 -0600

    partial completion of bishop moves

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,7 +7,7 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-
+    private ChessPiece[][] squares = new ChessPiece[8][8];
     public ChessBoard() {
         
     }
@@ -19,7 +19,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        squares[position.getRow()-1][position.getColumn()-1] = piece;
     }
 
     /**
@@ -30,7 +30,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return squares[position.getRow()-1][position.getColumn()-1];
     }
 
     /**
@@ -39,5 +39,13 @@ public class ChessBoard {
      */
     public void resetBoard() {
         throw new RuntimeException("Not implemented");
+    }
+
+    public boolean isWithinBoard(ChessPosition position) {
+        if(position.getRow() < 8 && position.getRow() > 1 && position.getColumn() < 8 && position.getColumn() > 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -10,7 +11,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,14 +35,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -46,7 +52,83 @@ public class ChessPiece {
      *
      * @return Collection of valid moves
      */
+
+//    public ArrayList<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
+//        ArrayList<ChessMove> pMoves = new ArrayList<>();
+//        //handle pawns moving one forward
+//        if()
+//    }
+//
+//    public ArrayList<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition) {
+//
+//    }
+//
+//    public ArrayList<ChessMove> knightMoves(ChessBoard board, ChessPosition myPosition) {
+//
+//    }
+
+    public ArrayList<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition) {
+        ArrayList<ChessMove> bMoves = new ArrayList<>();
+
+        int pieceDirections[][] = {
+                {1,1},
+                {-1,1},
+                {-1,-1},
+                {1,-1}
+        };
+
+        for(int [] direction : pieceDirections) {
+            int xShift = direction[0];
+            int yShift = direction[1];
+            ChessPosition startPosition = myPosition;
+            while(board.isWithinBoard(startPosition)) {
+                ChessPosition newPosition = startPosition.movePos(xShift,yShift);
+                if(board.getPiece(newPosition) == null) {
+                    bMoves.add(new ChessMove(myPosition,newPosition,null));
+                } else if(board.getPiece(newPosition).pieceColor != this.pieceColor) {
+                    bMoves.add(new ChessMove(myPosition,newPosition,null));
+                    break;
+                } else {
+                    break;
+                }
+                startPosition = newPosition;
+            }
+        }
+        return bMoves;
+    }
+
+//    public ArrayList<ChessMove> queenMoves(ChessBoard board, ChessPosition myPosition) {
+//
+//    }
+//
+//    public ArrayList<ChessMove> kingMoves(ChessBoard board, ChessPosition myPosition) {
+//
+//    }
+
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        PieceType type = board.getPiece(myPosition).getPieceType();
+
+        switch (type) {
+            case PAWN:
+//                moves = pawnMoves(board, myPosition);
+                break;
+            case ROOK:
+//                moves = rookMoves(board, myPosition);
+                break;
+            case KNIGHT:
+//                moves = knightMoves(board, myPosition);
+                break;
+            case BISHOP:
+                moves = bishopMoves(board, myPosition);
+                break;
+            case QUEEN:
+//                moves = queenMoves(board, myPosition);
+                break;
+            case KING:
+//                moves = kingMoves(board, myPosition);
+                break;
+        }
+        return moves;
     }
 }
