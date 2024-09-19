b261af5431bb5898a9ec514a480578d33f6e2971
Nathan Preslar <nathanpreslar@Nathans-MBP-35.app.byu.edu>
2024-09-18 20:19:17 -0600

    Pawn moves implemented

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -53,29 +53,66 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
 
+    public boolean isPromotionRow(ChessPosition myPosition) {
+        if(this.getTeamColor() == ChessGame.TeamColor.WHITE && myPosition.getRow() == 7) {
+            return true;
+        } else if(this.getTeamColor() == ChessGame.TeamColor.BLACK && myPosition.getRow() == 2) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFirstMove(ChessPosition myPosition) {
+        if(this.getTeamColor() == ChessGame.TeamColor.WHITE && myPosition.getRow() == 2) {
+            return true;
+        } else if(this.getTeamColor() == ChessGame.TeamColor.BLACK && myPosition.getRow() == 7) {
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<ChessMove> doPromotions(ArrayList<ChessMove> pMoves, ChessPosition myPosition, ChessPosition newPosition) {
+        pMoves.add(new ChessMove(myPosition,newPosition,PieceType.QUEEN));
+        pMoves.add(new ChessMove(myPosition,newPosition,PieceType.ROOK));
+        pMoves.add(new ChessMove(myPosition,newPosition,PieceType.BISHOP));
+        pMoves.add(new ChessMove(myPosition,newPosition,PieceType.KNIGHT));
+        return pMoves;
+    }
+
     public ArrayList<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessMove> pMoves = new ArrayList<>();
         int direction = board.getPiece(myPosition).getTeamColor() == ChessGame.TeamColor.WHITE ? 1 : -1;
-        int initialRow = board.getPiece(myPosition).getTeamColor() == ChessGame.TeamColor.WHITE ? 2 : 7;
-        int row = myPosition.getRow();
-        int col = myPosition.getColumn();
-        //handle pawns moving one and two forward
-        ChessPosition oneForward = new ChessPosition(row+1*direction, col);
-        if(board.getPiece(oneForward) == null) {
-            pMoves.add(new ChessMove(myPosition,oneForward,null));
-            ChessPosition twoForward = new ChessPosition(row+2*direction,col);
-            if(board.getPiece(twoForward) == null && myPosition.getRow() == initialRow) {
+        // handle one and two moves forward
+        ChessPosition oneForward = myPosition.movePos(1*direction,0);
+        ChessPosition twoForward = myPosition.movePos(2*direction,0);
+        if(board.isWithinBoard(oneForward) && board.getPiece(oneForward) == null) {
+            //one if that check for promotion
+            if(isPromotionRow(myPosition)) {
+                pMoves = doPromotions(pMoves,myPosition,oneForward);
+            } else {
+                pMoves.add(new ChessMove(myPosition,oneForward,null));
+            }
+            //if first move add two forward
+            if(isFirstMove(myPosition) && board.getPiece(twoForward) == null) {
                 pMoves.add(new ChessMove(myPosition,twoForward,null));
             }
         }
-        //handle diagonally taking another piece
-        ChessPosition leftDiagPos = new ChessPosition(row+1*direction, col-1);
-        if(board.getPiece(leftDiagPos) != null && board.getPiece(leftDiagPos).getTeamColor() != this.getTeamColor()) {
-            pMoves.add(new ChessMove(myPosition,leftDiagPos,null));
+        // account for attacking left and right with or without promotions
+        ChessPosition attackLeft = myPosition.movePos(1*direction,-1*direction);
+        ChessPosition attackRight = myPosition.movePos(1*direction,1*direction);
+        if(board.getPiece(attackLeft) != null && board.isEnemy(board,myPosition,attackLeft)) {
+            if(isPromotionRow(myPosition)) {
+                pMoves = doPromotions(pMoves,myPosition,attackLeft);
+            } else {
+                pMoves.add(new ChessMove(myPosition,attackLeft,null));
+            }
         }
-        ChessPosition rightDiagPos = new ChessPosition(row+1*direction, col+1);
-        if(board.getPiece(rightDiagPos) != null && board.getPiece(rightDiagPos).getTeamColor() != this.getTeamColor()) {
-            pMoves.add(new ChessMove(myPosition,rightDiagPos,null));
+        if(board.getPiece(attackRight) != null && board.isEnemy(board,myPosition,attackRight)) {
+            if(isPromotionRow(myPosition)) {
+                pMoves = doPromotions(pMoves,myPosition,attackRight);
+            } else {
+                pMoves.add(new ChessMove(myPosition,attackRight,null));
+            }
         }
         return pMoves;
     }
