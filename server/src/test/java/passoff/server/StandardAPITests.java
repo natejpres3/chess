e7898689741053595541abf9f1af911287e43997
Nathan Preslar <nathanpreslar@Nathans-MBP-35.v.byu.edu>
2024-11-01 18:54:34 -0600

    Code quality

--- server/src/test/java/passoff/server/StandardAPITests.java
+++ server/src/test/java/passoff/server/StandardAPITests.java
@@ -33,11 +33,7 @@ public class StandardAPITests {
 
     @BeforeAll
     public static void init() {
-//        try {
         server = new Server();
-//        } catch (DataAccessException e) {
-//            throw new RuntimeException(e);
-//        }
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
 
