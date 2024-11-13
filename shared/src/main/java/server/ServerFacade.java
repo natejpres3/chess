bc40863a49331835ed96d49a6020d104c13b002c
Nathan Preslar <nathanpreslar@Nathans-MBP-35.v.byu.edu>
2024-11-12 20:35:35 -0700

    implementing register in server facade

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,5 +1,13 @@
 package server;
 
+import com.google.gson.Gson;
+import model.AuthData;
+import model.UserData;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.*;
+
 public class ServerFacade {
     private final String serverUrl;
 
@@ -7,5 +15,38 @@ public class ServerFacade {
         serverUrl = url;
     }
 
-    
+    public AuthData register(UserData userData) {
+        var path = "/user";
+        return;
+    }
+
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+        } catch(Exception e) {
+
+        }
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if(request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException {
+        var status = http.getResponseCode();
+        if(!)
+    }
+
+
 }
