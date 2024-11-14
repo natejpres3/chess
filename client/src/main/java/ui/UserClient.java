1a36dd94ce369e71b07eaf569e875362d91bcb1e
Nathan Preslar <nathanpreslar@Nathans-MBP-35.v.byu.edu>
2024-11-13 18:33:46 -0700

    implemented user client functions

--- client/src/main/java/ui/UserClient.java
+++ client/src/main/java/ui/UserClient.java
@@ -29,12 +29,25 @@ public class UserClient {
         }
     }
 
+    public void register(String... params) throws Exception {
+        if(params.length != 3) {
+            System.out.println("Please provide your username, password, and email");
+            System.out.println("register <USERNAME> <PASSWORD> <EMAIL> - to create an account");
+        } else if(server.register(new UserData(params[0], params[1], params[2])) != null) {
+            System.out.println("You are registerd and logged in as " + params[0]);
+            isLoggedIn = true;
+        } else {
+            System.out.println("The username you chose is already in use, choose another");
+            System.out.println("register <USERNAME> <PASSWORD> <EMAIL> - to create an account");
+        }
+    }
+
     public void login(String... params) throws Exception {
         if(params.length != 2) {
             System.out.println("Please provide a username and password");
             System.out.println("login <USERNAME> <PASSWORD> - to play chess");
         } else if(server.login(new UserData(params[0],params[1], null)) != null) {
-            System.out.println("You are logged in as" + params[0]);
+            System.out.println("You are logged in as " + params[0]);
             isLoggedIn = true;
         } else {
             System.out.println("Username or password are incorrect, try again");
@@ -42,6 +55,10 @@ public class UserClient {
     }
 
     public void help() {
-        System.out.println("");
+        System.out.println("register <USERNAME> <PASSWORD> <EMAIL> - to create an account");
+        System.out.println("login <USERNAME> <PASSWORD> - to play chess");
+        System.out.println("quit - playing chess");
+        System.out.println("help - with possible commands");
     }
+
 }
