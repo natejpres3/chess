23fe1c038ec5778d4de7d4e9ea1a0640d03170ea
Nathan Preslar <nathanpreslar@Nathans-MacBook-Pro-35.local>
2024-11-14 05:24:35 -0700

    finished with the render board class

--- client/src/main/java/ui/RenderBoard.java
+++ client/src/main/java/ui/RenderBoard.java
@@ -2,7 +2,6 @@ package ui;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Random;
 
 import static ui.EscapeSequences.*;
 
@@ -10,7 +9,6 @@ public class RenderBoard {
     private static final int BOARD_SIZE_IN_SQUARES = 8;
     private static final int SQUARE_SIZE_IN_PADDED_CHARS = 5;
     private static final int LINE_WIDTH_IN_PADDED_CHARS = 1;
-    private static Random rand = new Random();
 
     public static void main(String[] args) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
@@ -24,8 +22,8 @@ public class RenderBoard {
         for(int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
             drawRow(out, boardRow);
             if(boardRow < BOARD_SIZE_IN_SQUARES-1) {
-                //drawhorizontalline
-                //setblack
+                drawHorizontal(out);
+                setBlack(out);
             }
         }
     }
@@ -33,7 +31,7 @@ public class RenderBoard {
     private static void drawRow(PrintStream out, int boardRow) {
         for(int squareRow = 0; squareRow < SQUARE_SIZE_IN_PADDED_CHARS; ++squareRow) {
             for(int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-                //set white(out)
+                setWhite(out);
                 if(squareRow == SQUARE_SIZE_IN_PADDED_CHARS / 2) {
                     int preLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
                     int suffLength = SQUARE_SIZE_IN_PADDED_CHARS - preLength - 1;
@@ -41,12 +39,88 @@ public class RenderBoard {
                     out.print(EMPTY.repeat(preLength));
 
                     if(boardRow == 0 || boardRow == 7) {
-                        out.print(//get chess piece);
+                        out.print(getPieces(boardRow, boardCol));
                     } else if(boardRow == 1 || boardRow == 6) {
-                        
+                        out.print(boardRow == 1 ? WHITE_PAWN : BLACK_PAWN);
+                    } else {
+                        out.print(EMPTY);
                     }
+                    out.print(EMPTY.repeat(suffLength));
+                } else {
+                    out.print(EMPTY.repeat(SQUARE_SIZE_IN_PADDED_CHARS));
                 }
+                //set vertical separators
+                if(boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+                    setRed(out);
+                    out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
+                }
+                setBlack(out);
+            }
+            out.println();
+        }
+    }
+
+    private static void drawHorizontal(PrintStream out) {
+        int spacesBoardSize = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_PADDED_CHARS +
+                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_PADDED_CHARS;
+        for(int lineRow = 0; lineRow < LINE_WIDTH_IN_PADDED_CHARS; ++lineRow) {
+            setRed(out);
+            out.print(EMPTY.repeat(spacesBoardSize));
+            setBlack(out);
+            out.println();
+        }
+    }
+
+    private static String getPieces(int row, int col) {
+        if(row == 0) {
+            switch (col) {
+                case 0:
+                case 7:
+                    return BLACK_ROOK;
+                case 1:
+                case 6:
+                    return BLACK_KNIGHT;
+                case 2:
+                case 5:
+                    return BLACK_BISHOP;
+                case 3:
+                    return BLACK_QUEEN;
+                case 4:
+                    return BLACK_KING;
             }
         }
+        if(row == 7) {
+            switch (col) {
+                case 0:
+                case 7:
+                    return WHITE_ROOK;
+                case 1:
+                case 6:
+                    return WHITE_KNIGHT;
+                case 2:
+                case 5:
+                    return WHITE_BISHOP;
+                case 3:
+                    return WHITE_QUEEN;
+                case 4:
+                    return WHITE_KING;
+            }
+        }
+        return EMPTY;
+    }
+
+    private static void setWhite(PrintStream out) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void setRed(PrintStream out) {
+        out.print(SET_BG_COLOR_RED);
+        out.print(SET_TEXT_COLOR_RED);
+    }
+
+    private static void setBlack(PrintStream out) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_BLACK);
     }
 }
